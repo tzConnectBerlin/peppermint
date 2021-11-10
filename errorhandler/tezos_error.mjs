c5089f24b1ff352455eba7e833c597c4990b616f
Daniel Jozsef <daniel.jozsef@gmail.com>
2021-11-10 18:22:03 +0100

    Refinements

--- errorhandler/tezos_error.mjs
+++ errorhandler/tezos_error.mjs
@@ -1,8 +1,6 @@
 
 const example = '[{"kind":"temporary","id":"failure","msg":"Error while applying operation ooSDQwbdToAQA83324mN26j4Mnp4hAyroq8NCk3PuFRTJ8XiQQu:\\nbranch delayed (Error:\\n                  { \\"id\\": \\"proto.010-PtGRANAD.gas_exhausted.block\\",\\n  \\"description\\":\\n    \\"The sum of gas consumed by all the operations in the block exceeds the hard gas limit per block\\",\\n  \\"data\\": {} }\\n)"}]\n'
 
-const parsed = JSON.parse(example);
-
 const parse_error_object = function(err) {
 	let msg = err.msg;
 	if (!msg) {
@@ -18,7 +16,18 @@ const parse_error_object = function(err) {
 	} catch {
 		return null;
 	}
-	return Object.assign({ msg_json: json_part }, err);
+	return Object.assign(json_part, { original_body: err });
 }
 
-console.log(parse_error_object(parsed[0]));
+const parse_rpc_error = function(json_body) {
+	let parsed_errors = json_body.map(parse_error_object);
+	if (parsed_errors.length == 1) {
+		return parsed_errors[0];
+	}
+	return {
+		id: 'multiple',
+		errors: parsed_errors
+	}
+}
+
+console.log(parse_rpc_error(JSON.parse(example)));
