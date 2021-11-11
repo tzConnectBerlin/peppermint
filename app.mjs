fe534f0b51e7db520b0842d9aaa657045b4cca70
Daniel Jozsef <daniel.jozsef@gmail.com>
2021-11-11 17:40:30 +0100

    Slightly better error handling

--- app.mjs
+++ app.mjs
@@ -1,10 +1,11 @@
 import { createRequire } from 'module'
-import { TezosToolkit } from '@taquito/taquito'
+import { TezosToolkit, TezosOperationError } from '@taquito/taquito'
 import { InMemorySigner } from '@taquito/signer'
 
 import Queue from './queue.mjs'
 import NftHandler from './operations/nft-multiasset.mjs'
 import TezHandler from './operations/tez.mjs'
+import { parse_rpc_error, postprocess_error_object } from './errorhandler/tezos_error.mjs'
 
 const require = createRequire(import.meta.url);
 require('console-stamp')(console);
@@ -64,28 +65,53 @@ const main = async function() {
 			return true;
 		}
 		console.log("Attempting to send operation group containing operations with ids:", JSON.stringify(batched_ids));
-		try {
-			let sent_operation = await batch.send();
-			let op_hash = sent_operation.opHash;
-			console.log("Sent operation group with hash", op_hash, "containing operations with ids:", JSON.stringify(batched_ids));
-			queue.save_sent(batched_ids, sent_operation.opHash).catch((err) => { console.error("Database error when writing hash to operations with ids:", JSON.stringify(batched_ids)); });
-			let result = await sent_operation.confirmation(config.confirmations);
-			if (result.completed) {
-				console.log("Operation group with hash", op_hash, "has been successfully confirmed.");
-				queue.save_confirmed(batched_ids).catch((err) => { console.error("Database error when saving confirmed status to operations with ids:", JSON.stringify(batched_ids)); });
-//				return true;
-			} else {
-				// FIXME: Taquito .confirmation() gives us some interesting and underdocumented results
-				// it should be possible to prepare for chain reorgs based on it
-				console.log("Operation group with hash", op_hash, "has failed.")
-				queue.save_failed(batched_ids).catch((err) => { console.error("Database error when saving failed status to operations with ids:", JSON.stringify(batched_ids)); });
+		while (true) {
+			try {
+				let sent_operation = await batch.send();
+				let op_hash = sent_operation.opHash;
+				console.log("Sent operation group with hash", op_hash, "containing operations with ids:", JSON.stringify(batched_ids));
+				queue.save_sent(batched_ids, sent_operation.opHash).catch((err) => { console.error("Database error when writing hash to operations with ids:", JSON.stringify(batched_ids)); });
+				let result = await sent_operation.confirmation(config.confirmations);
+				if (result.completed) {
+					console.log("Operation group with hash", op_hash, "has been successfully confirmed.");
+					queue.save_confirmed(batched_ids).catch((err) => { console.error("Database error when saving confirmed status to operations with ids:", JSON.stringify(batched_ids)); });
+	//				return true;
+				} else {
+					// FIXME: Taquito .confirmation() gives us some interesting and underdocumented results
+					// it should be possible to prepare for chain reorgs based on it
+					console.log("Operation group with hash", op_hash, "has failed.")
+					queue.save_failed(batched_ids).catch((err) => { console.error("Database error when saving failed status to operations with ids:", JSON.stringify(batched_ids)); });
+				}
+			} catch (err) {
+				let tezos_error = null;
+				if (TezosOperationError.prototype.isPrototypeOf(err)) {
+					tezos_error = postprocess_error_object(err);
+				} else {
+					tezos_error = parse_rpc_error(err.body);
+					console.log(tezos_error);
+				}
+				if (tezos_error) {
+					switch (tezos_error.id_noproto) {
+						// case "node.prevalidation.oversized_operation":
+						// case "gas_limit_too_high":
+						// 	// Do something smart later
+						case "gas_exhausted.block":
+							console.log("Block gas limit error - most likely due to know Octez bug. Retrying...")
+							continue;
+						case "michelson_v1.script_rejected":
+							console.log("A Michelson script error has occurred when processing operations with ids:", JSON.stringify(batched_ids), "\n", err);
+							break;
+						default:
+							console.log("A Tezos error has occurred when processing operations with ids:", JSON.stringify(batched_ids), "\n", err);
+					}
+					queue.save_rejected(batched_ids).catch((err) => { console.error("Database error when updating rejected operation with ids:", JSON.stringify(batched_ids)); });
+					return true;
+				} 
+				console.error("An error has occurred when processing operations with ids:", JSON.stringify(batched_ids), "\n", err, "\nOperation group state unknown.");
+				queue.save_unknown(batched_ids).catch((err) => { console.error("Database error when updating unknown state operation with ids:", JSON.stringify(batched_ids)); });;
+				return true;
 			}
-		} catch (err) {
-			console.error("An error has occurred when processing operations with ids:", JSON.stringify(batched_ids), "\n", err, "\nOperation group state unknown.");
-			queue.save_unknown(batched_ids).catch((err) => { console.error("Database error when updating rejected operation with ids:", JSON.stringify(batched_ids)); });;
-			// FIXME: add 'unknown' status
 		}
-		return true;
 	};
 
 	let signal = true;
--- errorhandler/tezos_error.mjs
+++ errorhandler/tezos_error.mjs
@@ -1,33 +1,51 @@
-
-const example = '[{"kind":"temporary","id":"failure","msg":"Error while applying operation ooSDQwbdToAQA83324mN26j4Mnp4hAyroq8NCk3PuFRTJ8XiQQu:\\nbranch delayed (Error:\\n                  { \\"id\\": \\"proto.010-PtGRANAD.gas_exhausted.block\\",\\n  \\"description\\":\\n    \\"The sum of gas consumed by all the operations in the block exceeds the hard gas limit per block\\",\\n  \\"data\\": {} }\\n)"}]\n'
+//const example_1 = '[{"kind":"temporary","id":"failure","msg":"Error while applying operation ooSDQwbdToAQA83324mN26j4Mnp4hAyroq8NCk3PuFRTJ8XiQQu:\\nbranch delayed (Error:\\n                  { \\"id\\": \\"proto.010-PtGRANAD.gas_exhausted.block\\",\\n  \\"description\\":\\n    \\"The sum of gas consumed by all the operations in the block exceeds the hard gas limit per block\\",\\n  \\"data\\": {} }\\n)"}]\n'
+//const example_2 = '[{"kind":"permanent","id":"node.prevalidation.oversized_operation","size":139096,"max_size":32768}]\n';
 
 const parse_error_object = function(err) {
 	let msg = err.msg;
-	if (!msg) {
+	if (msg) {
+		msg = msg.replace(/\n|\r/g, " ");
+		let json_part = msg.match(/{.*}/g);
+		if (!json_part) {
+			return null;
+		}
+		try {
+			json_part = JSON.parse(json_part);
+		} catch {
+			console.debug("Error while parsing node error message:", err);
+			return null;
+		}
+		return Object.assign(json_part, { container: err });
+	} else if (err.kind) {
+		return err;
+	} else {
 		return null;
 	}
-	msg = msg.replace(/\n|\r/g, " ");
-	let json_part = msg.match(/{.*}/g);
-	if (!json_part) {
-		return null;
-	}
+}
+
+export const postprocess_error_object = function(err) {
+	let id_noproto = err.id.replace(/proto\.[^.]+\./g, "");
+	return Object.assign({ id_noproto }, err);
+}
+
+export const parse_rpc_error = function(text_body) {
 	try {
-		json_part = JSON.parse(json_part);
-	} catch {
+		let json_body = JSON.parse(text_body);
+		let parsed_errors = json_body.map(parse_error_object).filter(e => e).map(postprocess_error_object);
+		switch (parsed_errors.length) {
+			case 0:
+				return null;
+			case 1:
+				return Object.assign(parsed_errors[0]);
+			default:
+				return {
+					id: 'multiple',
+					errors: parsed_errors
+				}
+		}
+	} catch (err) {
+		console.debug("Error while parsing error message:", err);
 		return null;
 	}
-	return Object.assign(json_part, { original_body: err });
 }
 
-const parse_rpc_error = function(json_body) {
-	let parsed_errors = json_body.map(parse_error_object);
-	if (parsed_errors.length == 1) {
-		return parsed_errors[0];
-	}
-	return {
-		id: 'multiple',
-		errors: parsed_errors
-	}
-}
-
-console.log(parse_rpc_error(JSON.parse(example)));
