4005b938886e405ac8de83031b36d232f8e8e547
Daniel Jozsef <daniel.jozsef@gmail.com>
2021-12-10 14:17:50 +0100

    Remint added

--- operations/nft-multiasset.mjs
+++ operations/nft-multiasset.mjs
@@ -1,5 +1,5 @@
 //import { TezosToolkit } from "@taquito/taquito";
-import { MichelsonMap } from '@taquito/taquito'
+import { MichelsonMap, TezosPreapplyFailureError } from '@taquito/taquito'
 import { char2Bytes } from '@taquito/utils'
 // import { createRequire } from 'module'
 // const require = createRequire(import.meta.url);
@@ -27,15 +27,22 @@ export default async function(tezos, nft_address) {
 		let token_info = MichelsonMap.fromLiteral({"": char2Bytes(metadata_ipfs)});
 		let create_op = contract_ops.create_token(token_id, token_info);
 		return create_op;
-	}
+	};
+
+	let mint_token = function(token_id, to_address, amount = 1) {
+		let mint_op = contract_ops.mint_tokens([{ owner: to_address, token_id, amount }]);
+		return mint_op;
+	};
 
 	return {
-		mint: function({ token_id, to_address, metadata_ipfs, amount }, batch) {
-			if (!amount) {
-				amount = 1;
-			}
+		create: function({ token_id, metadata_ipfs }, batch) {
 			let create_op = create_token(token_id, metadata_ipfs);
-			let mint_op = contract_ops.mint_tokens([{ owner: to_address, token_id, amount }]);
+			batch.withContractCall(create_op);
+			return true;
+		},
+		mint: function({ token_id, to_address, metadata_ipfs, amount }, batch) {
+			let create_op = create_token(token_id, metadata_ipfs);
+			let mint_op = mint_token(token_id, to_address, amount);
 			batch.withContractCall(create_op);
 			batch.withContractCall(mint_op);
 			return true;
@@ -45,6 +52,11 @@ export default async function(tezos, nft_address) {
 			batch.withContractCall(create_op);
 			let mint_args = destinations.map(e => ({ token_id, owner: e.to_address, amount: e.amount }));
 			let mint_op = contract_ops.mint_tokens(mint_args);
+			batch.withContractCall(mint_op);
+			return true;
+		},
+		remint: function({ token_id, to_address, amount }, batch) {
+			let mint_op = mint_token(token_id, to_address, amount);
 			batch.withContractCall(mint_op);
 			return true;
 		},
@@ -64,7 +76,7 @@ export default async function(tezos, nft_address) {
                     ]
                 }
             ];
-			batch.withContractCall(transfer_tokens(transfer_arg));
+			batch.withContractCall(contract_ops.transfer_tokens(transfer_arg));
 			return true;
 		}
 	};
