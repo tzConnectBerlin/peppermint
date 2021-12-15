6f1a3d933dc4b68727dd5a5eb5aaf8ee49da3235
Daniel Jozsef <daniel.jozsef@gmail.com>
2021-12-15 16:41:15 +0100

    Beauiful refactor

--- app.mjs
+++ app.mjs
@@ -6,12 +6,15 @@ import Queue from './queue.mjs'
 import NftHandler from './operations/nft-multiasset.mjs'
 import TezHandler from './operations/tez.mjs'
 import { parse_rpc_error, postprocess_error_object } from './errorhandler/tezos_error.mjs'
+import tez from './operations/tez.mjs'
 
 const require = createRequire(import.meta.url);
 require('console-stamp')(console);
 const config = require('./config.json');
 
 const main = async function() {
+	let batch_divider = 1;
+
 	const queue = Queue(config.dbConnection);
 	const tezos = new TezosToolkit(config.rpcUrl);
 	const signer = new InMemorySigner(config.privateKey);
@@ -36,8 +39,12 @@ const main = async function() {
 		return false;
 	}
 
+	const save_state_async = function(ids, state) {
+		queue.save_state(ids, state).catch((err) => { console.error("Database error when setting", state, "on operation with ids:", JSON.stringify(ids)); });;
+	}
+
 	const heartbeat = async function() {
-		let ops = await queue.checkout(address, config.batchSize);
+		let ops = await queue.checkout(address, ~~(config.batchSize/batch_divider) + 1);
 		if (ops.length == 0) {
 			console.log("No pending operations for originator", address);
 			return true;
@@ -56,65 +63,71 @@ const main = async function() {
 			}
 		}));
 
+		// Ideally the handlers should have filtered out obviously bad ops
 		if (rejected_ids.length > 0) {
 			console.warn('Rejected operations with ids:', JSON.stringify(rejected_ids));
-			queue.save_rejected(rejected_ids).catch((err) => { console.error("Database error when updating rejected operation with ids:", JSON.stringify(rejected_ids)); });;
+			save_state_async(rejected_ids, queue.state.REJECTED);
 		}
 		if (batched_ids.length == 0) {
 			console.log("No valid operations left, aborting batch.");
 			return true;
 		}
+
 		console.log("Attempting to send operation group containing operations with ids:", JSON.stringify(batched_ids));
-		while (true) {
-			try {
-				let sent_operation = await batch.send();
-				let op_hash = sent_operation.opHash;
-				console.log("Sent operation group with hash", op_hash, "containing operations with ids:", JSON.stringify(batched_ids));
-				queue.save_sent(batched_ids, sent_operation.opHash).catch((err) => { console.error("Database error when writing hash to operations with ids:", JSON.stringify(batched_ids)); });
-				let result = await sent_operation.confirmation(config.confirmations);
-				if (result.completed) {
-					console.log("Operation group with hash", op_hash, "has been successfully confirmed.");
-					queue.save_confirmed(batched_ids).catch((err) => { console.error("Database error when saving confirmed status to operations with ids:", JSON.stringify(batched_ids)); });
-					return true;
-				} else {
-					// FIXME: Taquito .confirmation() gives us some interesting and underdocumented results
-					// it should be possible to prepare for chain reorgs based on it
-					console.log("Operation group with hash", op_hash, "has failed.")
-					queue.save_failed(batched_ids).catch((err) => { console.error("Database error when saving failed status to operations with ids:", JSON.stringify(batched_ids)); });
-				}
-			} catch (err) {
-				let tezos_error = null;
-				if (TezosOperationError.prototype.isPrototypeOf(err)) {
-					tezos_error = postprocess_error_object(err);
-				} else {
-					tezos_error = parse_rpc_error(err.body);
-					console.log(tezos_error);
-				}
-				if (tezos_error) {
-					switch (tezos_error.id_noproto) {
-						// case "node.prevalidation.oversized_operation":
-						// case "gas_limit_too_high":
-						// 	// Do something smart later
-						case "gas_exhausted.block":
-							console.log("Block gas limit error - most likely due to know Octez bug. Retrying...");
-							continue;
-						case "contract.balance_too_low":
-						case "contract.cannot_pay_storage_fee":
-							console.log("Balance too low, please fund the account. Retrying...");
-							continue;
-						case "michelson_v1.script_rejected":
-							console.log("A Michelson script error has occurred when processing operations with ids:", JSON.stringify(batched_ids), "\n", err);
-							break;
-						default:
-							console.log("A Tezos error has occurred when processing operations with ids:", JSON.stringify(batched_ids), "\n", err);
-					}
-					queue.save_rejected(batched_ids).catch((err) => { console.error("Database error when updating rejected operation with ids:", JSON.stringify(batched_ids)); });
-					return true;
-				} 
-				console.error("An error has occurred when processing operations with ids:", JSON.stringify(batched_ids), "\n", err, "\nOperation group state unknown.");
-				queue.save_unknown(batched_ids).catch((err) => { console.error("Database error when updating unknown state operation with ids:", JSON.stringify(batched_ids)); });;
+		try {
+			let sent_operation = await batch.send();
+			let op_hash = sent_operation.opHash;
+			console.log("Sent operation group with hash", op_hash, "containing operations with ids:", JSON.stringify(batched_ids));
+			
+			// Save operation group hash - async because it's okay if it happens later
+			queue.save_sent(batched_ids, sent_operation.opHash).catch( (err) => { console.error("Database error when writing hash", sent_operation.opHash, "to sent operations with ids:", JSON.stringify(batched_ids)); } );
+
+			// Wait for it to be baked
+			let result = await sent_operation.confirmation(config.confirmations);
+			if (result.completed) {
+				console.log("Operation group with hash", op_hash, "has been successfully confirmed.");
+				save_state_async(batched_ids, queue.state.CONFIRMED); // save state
+				batch_divider = 1; // reset batch divider
 				return true;
+			} else {
+				// FIXME: Taquito .confirmation() gives us some interesting and underdocumented results
+				// it should be possible to prepare for chain reorgs based on it
+				console.error("Operation group with hash", op_hash, "has failed on chain.")
+				save_state_async(batched_ids, queue.state.FAILED);
 			}
+		} catch (err) {
+			let tezos_error = null;
+			if (TezosOperationError.prototype.isPrototypeOf(err)) {
+				tezos_error = postprocess_error_object(err);
+			} else {
+				tezos_error = parse_rpc_error(err.body);
+			}
+			if (tezos_error) {
+				switch (tezos_error.id_noproto) {
+					case "node.prevalidation.oversized_operation":
+					case "gas_limit_too_high":
+						// Operation group too big
+						batch_divider += 1; // Cut batch size
+					case "gas_exhausted.block":
+						// Octez is having an anxiety attack
+					case "contract.balance_too_low":
+					case "contract.cannot_pay_storage_fee":
+						// Account has no tez in it
+						console.warn("Retriable Tezos error encountered:\n", tezos_error, "\nRetrying operations with ids:", JSON.stringify(batched_ids));
+						await queue.save_state(batched_ids, queue.state.PENDING);
+						break;
+					case "michelson_v1.script_rejected":
+						// The call failed on some business logic check in the contract
+					default:
+						// Everything else
+						console.error("Non-retriable Tezos error encountered:\n", tezos_error, "\nRejecting operations with ids:", JSON.stringify(batched_ids));
+						save_state_async(batched_ids, queue.state.REJECTED);
+				}
+				return true;
+			} 
+			console.error("An unhandled error has occurred when processing operations with ids:", JSON.stringify(batched_ids), "\n", err, "\nOperation group state unknown.");
+			save_state_async(batched_ids, queue.state.UNKNOWN);
+			return true;
 		}
 	};
 
