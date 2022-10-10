b24f414013f0169afdd0e4bb25e72730baca59d1
Joris Röling <joris@bonboa.com>
2022-10-10 10:58:47 +0200

    WIP

--- insert_example.sql
+++ insert_example.sql
@@ -1,1 +1,15 @@
 insert into peppermint.operations (originator , command) values ('tz1Pw1WizZhFx3dz6mLh28g7ZS4GrLEXLhoe', '{"args": {"token_id": 2, "to_address": "tz2QA1mGkeibTakCrNbaEdVwsWFm2BELQnuv", "metadata_ipfs": "ipfs://QmQoV15JrBWB4NjBVUAzeTq6CZsmoWXnt9ABSmupcrd6dg"}, "name": "create_and_mint", "handler": "nft"}');
+
+
+-- {
+--   "originator": "tz1Pw1WizZhFx3dz6mLh28g7ZS4GrLEXLhoe",
+--   "command": {
+--     "args": {
+--       "token_id": 2,
+--       "to_address": "tz2QA1mGkeibTakCrNbaEdVwsWFm2BELQnuv",
+--       "metadata_ipfs": "ipfs://QmQoV15JrBWB4NjBVUAzeTq6CZsmoWXnt9ABSmupcrd6dg"
+--     },
+--     "name": "create_and_mint",
+--     "handler": "nft"
+--   }
+-- }
