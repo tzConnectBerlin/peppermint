72c70c6c13c16bf2abc11ff565134744bb9e9208
John Newby <john@newby.org>
2022-10-10 16:28:01 +0200

    Don't hate the player, hate the game.

--- confloader.mjs
+++ confloader.mjs
@@ -4,8 +4,13 @@ const require = createRequire(import.meta.url);
 const CONFIG_PATH = '.'
 
 export default function() {
-	let profile = process.env.PEPPERMINT_PROFILE;
-	let config_filename = profile ? `config_${profile}.json` : 'config.json';
-	console.log(`Reading configuration from ${config_filename}...`);
-	return require(`${CONFIG_PATH}/${config_filename}`);
+  let config_json = process.env.PEPPERMINT_CONFIG;
+  if(config_json) {
+    console.log(config_json);
+    return JSON.parse(config_json);
+  }
+  let profile = process.env.PEPPERMINT_PROFILE;
+  let config_filename = profile ? `config_${profile}.json` : 'config.json';
+  console.log(`Reading configuration from ${config_filename}...`);
+  return require(`${CONFIG_PATH}/${config_filename}`);
 }
