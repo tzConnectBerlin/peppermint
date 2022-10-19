5623296c87262c8731f842c129298d17c8e990db
John Newby <john@newby.org>
2022-10-19 11:08:04 +0200

    Needs work!

--- app.mjs
+++ app.mjs
@@ -9,8 +9,8 @@ import TezHandler from './operations/tez.mjs'
 import ConfLoader from './confloader.mjs'
 
 const Handlers = {
-	MultiassetHandler,
-	TezHandler
+  MultiassetHandler,
+  TezHandler
 }
 
 const require = createRequire(import.meta.url);
@@ -40,142 +40,147 @@ const main = async function() {
   let batch_divider = 1;
 
   const signer = await get_signing_key(config)
-	const pool = new Pool(config.dbConnection);
+  const pool = new Pool(config.dbConnection);
   const queue = Queue(pool, signer.publicKeyHash());
   const tezos = new TezosToolkit(config.rpcUrl);
 
   console.log("signer: " + signer);
   const address = await signer.publicKeyHash();
   console.log("Signer initialized for originating address ", address);
-	tezos.setSignerProvider(signer);
+  tezos.setSignerProvider(signer);
 
-	let handlers = {};
-	for (let key in config.handlers) {
-		let val = config.handlers[key];
-		handlers[key] = await (Handlers[val.handler](tezos, val.args, pool));
+  let handlers = {};
+  for (let key in config.handlers) {
+    let val = config.handlers[key];
+    handlers[key] = await (Handlers[val.handler](tezos, val.args, pool));
+  }
+
+  const dispatch_command = function(command, batch) {
+    let handler = handlers[command.handler];
+    if (handler) {
+      let handling_function = handler[command.name];
+      if (handling_function) {
+	return handling_function(command.args, batch);
+      }
+    }
+    console.warn("Invalid comand:", JSON.stringify(command));
+    return false;
+  }
+
+  const save_state_async = function(ids, state) {
+    queue.save_state(ids, state).catch((err) => { console.error("Database error when setting", state, "on operation with ids:", JSON.stringify(ids)); });;
+  }
+
+  const heartbeat = async function() {
+    await queue.kill_canaries();
+
+    let ops = await queue.checkout(address, ~~(config.batchSize/batch_divider) + 1);
+    if (ops.length == 0) {
+      console.log("No pending operations for originator", address);
+      return true;
+    }
+
+    console.log("Generating batch with", ops.length, "operations.")
+    let batch = tezos.wallet.batch();
+    let batched_ids = [];
+    let rejected_ids = [];
+    await Promise.all(ops.map((operation) => {
+
+      let success = dispatch_command(operation.command, batch);
+
+
+      if (success) {
+	batched_ids.push(operation.id);
+      } else {
+	rejected_ids.push(operation.id);
+      }
+    }));
+
+    // Ideally the handlers should have filtered out obviously bad ops
+    if (rejected_ids.length > 0) {
+      console.warn('Rejected operations with ids:', JSON.stringify(rejected_ids));
+      save_state_async(rejected_ids, queue.state.REJECTED);
+    }
+    if (batched_ids.length == 0) {
+      console.log("No valid operations left, aborting batch.");
+      return true;
+    }
+
+    console.log("Attempting to send operation group containing operations with ids:", JSON.stringify(batched_ids));
+    try {
+            console.log("foo");
+      let sent_operation = await batch.send();
+            console.log("bar");
+      let op_hash = sent_operation.opHash;
+      console.log("Sent operation group with hash", op_hash, "containing operations with ids:", JSON.stringify(batched_ids));
+
+      // Save operation group hash - async because it's okay if it happens later
+      queue.save_sent(batched_ids, sent_operation.opHash).catch( (err) => { console.error("Database error when writing hash", sent_operation.opHash, "to sent operations with ids:", JSON.stringify(batched_ids)); } );
+
+      // Wait for it to be baked
+      let result = await sent_operation.confirmation(config.confirmations, config.timeout);
+      if (result.completed) {
+	console.log("Operation group with hash", op_hash, "has been successfully confirmed.");
+	save_state_async(batched_ids, queue.state.CONFIRMED); // save state
+	batch_divider = 1; // reset batch divider
+	return true;
+      } else {
+	// FIXME: Taquito .confirmation() gives us some interesting and underdocumented results
+	// it should be possible to prepare for chain reorgs based on it
+	console.error("Operation group with hash", op_hash, "has failed on chain.")
+	save_state_async(batched_ids, queue.state.FAILED);
+      }
+    } catch (err) {
+      let tezos_error = null;
+      if (TezosOperationError.prototype.isPrototypeOf(err)) {
+	tezos_error = postprocess_error_object(err);
+      } else {
+	tezos_error = parse_rpc_error(err.body);
+      }
+      if (tezos_error) {
+	switch (tezos_error.id_noproto) {
+	case "node.prevalidation.oversized_operation":
+	case "gas_limit_too_high":
+	  // Operation group too big
+	  batch_divider += 1; // Cut batch size
+	case "gas_exhausted.block":
+	  // Octez is having an anxiety attack
+	case "contract.balance_too_low":
+	case "contract.cannot_pay_storage_fee":
+	  // Account has no tez in it
+	case "storage_exhausted.operation":
+	  // There seems to be a bug in the node that may give this error when the account is low on tez
+	  console.warn("Retriable Tezos error encountered:\n", tezos_error, "\nRetrying operations with ids:", JSON.stringify(batched_ids));
+	  await queue.save_state(batched_ids, queue.state.PENDING);
+	  break;
+	case "michelson_v1.script_rejected":
+	  // The call failed on some business logic check in the contract
+	default:
+	  // Everything else
+	  console.error("Non-retriable Tezos error encountered:\n", tezos_error, "\nRejecting operations with ids:", JSON.stringify(batched_ids));
+	  save_state_async(batched_ids, queue.state.REJECTED);
 	}
+	return true;
+      }
+      console.error("An unhandled error has occurred when processing operations with ids:", JSON.stringify(batched_ids), "\n", err, "\nOperation group state unknown.");
+      save_state_async(batched_ids, queue.state.UNKNOWN);
+      return true;
+    }
+  };
 
-	const dispatch_command = function(command, batch) {
-		let handler = handlers[command.handler];
-		if (handler) {
-			let handling_function = handler[command.name];
-			if (handling_function) {
-				return handling_function(command.args, batch);
-			}
-		}
-		console.warn("Invalid comand:", JSON.stringify(command));
-		return false;
-	}
-
-	const save_state_async = function(ids, state) {
-		queue.save_state(ids, state).catch((err) => { console.error("Database error when setting", state, "on operation with ids:", JSON.stringify(ids)); });;
-	}
-
-	const heartbeat = async function() {
-		await queue.kill_canaries();
-
-		let ops = await queue.checkout(address, ~~(config.batchSize/batch_divider) + 1);
-		if (ops.length == 0) {
-			console.log("No pending operations for originator", address);
-			return true;
-		}
-
-		console.log("Generating batch with", ops.length, "operations.")
-		let batch = tezos.wallet.batch();
-		let batched_ids = [];
-		let rejected_ids = [];
-		await Promise.all(ops.map((operation) => {
-			let success = dispatch_command(operation.command, batch);
-			if (success) {
-				batched_ids.push(operation.id);
-			} else {
-				rejected_ids.push(operation.id);
-			}
-		}));
-
-		// Ideally the handlers should have filtered out obviously bad ops
-		if (rejected_ids.length > 0) {
-			console.warn('Rejected operations with ids:', JSON.stringify(rejected_ids));
-			save_state_async(rejected_ids, queue.state.REJECTED);
-		}
-		if (batched_ids.length == 0) {
-			console.log("No valid operations left, aborting batch.");
-			return true;
-		}
-
-		console.log("Attempting to send operation group containing operations with ids:", JSON.stringify(batched_ids));
-		try {
-			let sent_operation = await batch.send();
-			let op_hash = sent_operation.opHash;
-			console.log("Sent operation group with hash", op_hash, "containing operations with ids:", JSON.stringify(batched_ids));
-
-			// Save operation group hash - async because it's okay if it happens later
-			queue.save_sent(batched_ids, sent_operation.opHash).catch( (err) => { console.error("Database error when writing hash", sent_operation.opHash, "to sent operations with ids:", JSON.stringify(batched_ids)); } );
-
-			// Wait for it to be baked
-			let result = await sent_operation.confirmation(config.confirmations, config.timeout);
-			if (result.completed) {
-				console.log("Operation group with hash", op_hash, "has been successfully confirmed.");
-				save_state_async(batched_ids, queue.state.CONFIRMED); // save state
-				batch_divider = 1; // reset batch divider
-				return true;
-			} else {
-				// FIXME: Taquito .confirmation() gives us some interesting and underdocumented results
-				// it should be possible to prepare for chain reorgs based on it
-				console.error("Operation group with hash", op_hash, "has failed on chain.")
-				save_state_async(batched_ids, queue.state.FAILED);
-			}
-		} catch (err) {
-			let tezos_error = null;
-			if (TezosOperationError.prototype.isPrototypeOf(err)) {
-				tezos_error = postprocess_error_object(err);
-			} else {
-				tezos_error = parse_rpc_error(err.body);
-			}
-			if (tezos_error) {
-				switch (tezos_error.id_noproto) {
-					case "node.prevalidation.oversized_operation":
-					case "gas_limit_too_high":
-						// Operation group too big
-						batch_divider += 1; // Cut batch size
-					case "gas_exhausted.block":
-						// Octez is having an anxiety attack
-					case "contract.balance_too_low":
-					case "contract.cannot_pay_storage_fee":
-						// Account has no tez in it
-					case "storage_exhausted.operation":
-						// There seems to be a bug in the node that may give this error when the account is low on tez
-						console.warn("Retriable Tezos error encountered:\n", tezos_error, "\nRetrying operations with ids:", JSON.stringify(batched_ids));
-						await queue.save_state(batched_ids, queue.state.PENDING);
-						break;
-					case "michelson_v1.script_rejected":
-						// The call failed on some business logic check in the contract
-					default:
-						// Everything else
-						console.error("Non-retriable Tezos error encountered:\n", tezos_error, "\nRejecting operations with ids:", JSON.stringify(batched_ids));
-						save_state_async(batched_ids, queue.state.REJECTED);
-				}
-				return true;
-			}
-			console.error("An unhandled error has occurred when processing operations with ids:", JSON.stringify(batched_ids), "\n", err, "\nOperation group state unknown.");
-			save_state_async(batched_ids, queue.state.UNKNOWN);
-			return true;
-		}
-	};
-
-	let signal = true;
-	while (signal) {
-		try {
-			let [ result, _ ] = await Promise.all([
-				heartbeat(),
-				new Promise(_ => setTimeout(_, config.pollingDelay))
-			]);
-			signal = result;
-		} catch (err) {
-			console.error("An error has occurred in the main event loop.\n", err);
-			signal = false;
-		}
-	}
+  let signal = true;
+  while (signal) {
+    try {
+      let [ result, _ ] = await Promise.all([
+	heartbeat(),
+	new Promise(_ => setTimeout(_, config.pollingDelay))
+      ]);
+      signal = result;
+    } catch (err) {
+      console.error("An error has occurred in the main event loop.\n", err);
+      signal = false;
+    }
+  }
 
 };
 
--- operations/nft-multiasset.mjs
+++ operations/nft-multiasset.mjs
@@ -1,144 +1,135 @@
-//import { TezosToolkit } from "@taquito/taquito";
+// -*-javascript-*-
 import { MichelsonMap, TezosPreapplyFailureError } from '@taquito/taquito'
 import { char2Bytes } from '@taquito/utils'
-// import { createRequire } from 'module'
-// const require = createRequire(import.meta.url);
-
-// const hex = require('string-hex');
-// const utf8 = require('utf8')
 
 export default async function(tezos, { contract_address }, pool) {
-	let nft_contract = await tezos.contract.at(contract_address);
-	console.log("token contract loaded", nft_contract.parameterSchema.ExtractSignatures());
+  let nft_contract = await tezos.contract.at(contract_address);
+  console.log("token contract loaded", nft_contract.parameterSchema.ExtractSignatures());
 
-	let contract_ops = {
-		create_token: nft_contract.methods.create_token,
-		mint_tokens: nft_contract.methods.mint_tokens,
-		transfer_tokens: nft_contract.methods.transfer
-	}
+  let contract_ops = {
+    create_token: nft_contract.methods.create_token,
+    mint_tokens: nft_contract.methods.mint_tokens,
+    transfer_tokens: nft_contract.methods.transfer
+  }
 
-	// Object.entries(contract_ops).forEach(([key, value]) => {
-	// 	if (typeof value != 'function') {
-	// 		throw new Error("Invalid token contract signature");
-	// 	}
-	// });
+  let create_token = function(token_id, metadata_ipfs) {
+    if (typeof contract_ops.create_token != 'function') {
+      throw new Error("No create_token entrypoint on contract");
+    }
+    let token_info = MichelsonMap.fromLiteral({"": char2Bytes(metadata_ipfs)});
+    let create_op = contract_ops.create_token(token_id, token_info);
+    return create_op;
+  };
 
-	let create_token = function(token_id, metadata_ipfs) {
-		if (typeof contract_ops.create_token != 'function') {
-			throw new Error("No create_token entrypoint on contract");
-		}
-		let token_info = MichelsonMap.fromLiteral({"": char2Bytes(metadata_ipfs)});
-		let create_op = contract_ops.create_token(token_id, token_info);
-		return create_op;
-	};
+  let mint_token = function(token_id, to_address, amount = 1) {
+    if (typeof contract_ops.mint_tokens != 'function') {
+      throw new Error("No mint_tokens entrypoint on contract");
+    }
+    let mint_op = contract_ops.mint_tokens([{ owner: to_address, token_id, amount }]);
+    return mint_op;
+  };
 
-	let mint_token = function(token_id, to_address, amount = 1) {
-		if (typeof contract_ops.mint_tokens != 'function') {
-			throw new Error("No mint_tokens entrypoint on contract");
-		}
-		let mint_op = contract_ops.mint_tokens([{ owner: to_address, token_id, amount }]);
-		return mint_op;
-	};
+  let burn_token = function(token_id, from_address, amount = 1) {
+    if (typeof contract_ops.burn_tokens != 'function') {
+      throw new Error("No mint_tokens entrypoint on contract");
+    }
+    let burn_op = contract_ops.burn_tokens([{ owner: from_address, token_id, amount }]);
+    return burn_op;
+  };
 
-	let burn_token = function(token_id, from_address, amount = 1) {
-		if (typeof contract_ops.burn_tokens != 'function') {
-			throw new Error("No mint_tokens entrypoint on contract");
-		}
-		let burn_op = contract_ops.burn_tokens([{ owner: from_address, token_id, amount }]);
-		return burn_op;
-	};
+  const transfer = function({ token_id, from_address, to_address, amount }, batch) {
+    if (typeof contract_ops.transfer_tokens != 'function') {
+      throw new Error("No transfer_tokens entrypoint on contract");
+    }
+    if (!amount) {
+      amount = 1;
+    }
+    let transfer_arg = [
+      {
+	from_: from_address,
+	txs: [
+	  {
+	    to_: to_address,
+	    token_id,
+	    amount
+	  }
+	]
+      }
+    ];
+    console.log(JSON.stringify(transfer_arg));
+    batch.withContractCall(contract_ops.transfer_tokens(transfer_arg));
+    return true;
+  }
 
-	const transfer = function({ token_id, from_address, to_address, amount }, batch) {
-			if (typeof contract_ops.transfer_tokens != 'function') {
-				throw new Error("No transfer_tokens entrypoint on contract");
-			}
-			if (!amount) {
-				amount = 1;
-			}
-			let transfer_arg = [
-				{
-					from_: from_address,
-					txs: [
-						{
-							to_: to_address,
-							token_id,
-							amount
-						}
-					]
-				}
-			];
-			batch.withContractCall(contract_ops.transfer_tokens(transfer_arg));
-			return true;
-	}
+  const get_next_from_table = async function({from_address, to_address}, batch) {
+    const client = await pool.connect();
 
-	let do_thing = async function(from_address, to_address, batch) {
-		const client = await pool.connect();
+    const FIND_UNALLOCATED_ROW = "SELECT * FROM nfts WHERE recipient IS NULL ORDER BY id LIMIT 1"
+    const UPDATE_RECIPIENT_SQL = "UPDATE nfts SET recipient = $1 WHERE id = $2 AND recipient IS NULL"
 
-		const FIND_UNALLOCATED_ROW = "SELECT * FROM nfts WHERE recipient IS NULL ORDER BY id LIMIT 1"
-		const UPDATE_RECIPIENT_SQL = "UPDATE nfts SET recipient = $1 WHERE token_id = $2 AND recipient IS NULL"
+    try {
+      const unallocatedRowResult =  await client.query(FIND_UNALLOCATED_ROW);
+      const unallocatedRows = unallocatedRowResult.rows;
 
-		try {
-			const unallocatedRowResult =  await client.query(FIND_UNALLOCATED_ROW);
-			const unallocatedRows = unallocatedRowResult.rows;
+      if (unallocatedRows.length !== 1) {
+	throw new Error('Could not get an unallocated row')
+      }
+      const { id: row_id, token_id: unallocatedTokenId } = unallocatedRows[0];
 
-			if (unallocatedRows.length !== 1) {
-				throw new Error('Could not get an unallocated row')
-			}
+      const values = [to_address, row_id];
+      await client.query('BEGIN');
+      const result = await client.query(UPDATE_RECIPIENT_SQL, values);
 
-			const { token_id: unallocatedTokenId } = unallocatedRows[0];
+      const updateSuccessful = result.rowCount === 1;
+      if (!updateSuccessful) {
+	throw new Error('No row updated, transaction aborted');
+      }
 
-			const values = [to_address, unallocatedTokenId];
-			await client.query('BEGIN');
-			const result = await client.query(UPDATE_RECIPIENT_SQL, values);
+      transfer({ token_id: unallocatedTokenId, from_address, to_address, amount:1 }, batch);
 
-			const updateSuccessful = result.rowCount === 1;
+      await client.query('COMMIT');
+    } catch (error) {
+      console.log("Error: " + error);
+      await client.query('ROLLBACK');
+      throw error;
+    } finally {
+      client.release()
+    }
+    return true;
+  }
 
-			if (!updateSuccessful) {
-				throw new Error('No row updated, transaction aborted');
-			}
-
-			transfer({ token_id: unallocatedTokenId, from_address, to_address }, batch);
-
-			await client.query('COMMIT');
-		} catch (error) {
-			await client.query('ROLLBACK');
-			throw error;
-		} finally {
-			client.release()
-		}
-	}
-
-	return {
-		create: function({ token_id, metadata_ipfs }, batch) {
-			let create_op = create_token(token_id, metadata_ipfs);
-			batch.withContractCall(create_op);
-			return true;
-		},
-		mint: function({ token_id, to_address, amount }, batch) {
-			let mint_op = mint_token(token_id, to_address, amount);
-			batch.withContractCall(mint_op);
-			return true;
-		},
-		create_and_mint: function({ token_id, to_address, metadata_ipfs, amount }, batch) {
-			let create_op = create_token(token_id, metadata_ipfs);
-			let mint_op = mint_token(token_id, to_address, amount);
-			batch.withContractCall(create_op);
-			batch.withContractCall(mint_op);
-			return true;
-		},
-		create_and_mint_multiple: function({ token_id, metadata_ipfs, destinations }, batch) {
-			let create_op = create_token(token_id, metadata_ipfs);
-			let mint_args = destinations.map(e => ({ token_id, owner: e.to_address, amount: e.amount }));
-			let mint_op = contract_ops.mint_tokens(mint_args);
-			batch.withContractCall(create_op);
-			batch.withContractCall(mint_op);
-			return true;
-		},
-		transfer,
-		burn: function({ token_id, from_address, amount }, batch) {
-			let burn_op = burn_token(token_id, from_address, amount);
-			batch.withContractCall(burn_op);
-			return true;
-		},
-	};
+  return {
+    burn: function({ token_id, from_address, amount }, batch) {
+      let burn_op = burn_token(token_id, from_address, amount);
+      batch.withContractCall(burn_op);
+      return true;
+    },
+    create: function({ token_id, metadata_ipfs }, batch) {
+      let create_op = create_token(token_id, metadata_ipfs);
+      batch.withContractCall(create_op);
+      return true;
+    },
+    create_and_mint: function({ token_id, to_address, metadata_ipfs, amount }, batch) {
+      let create_op = create_token(token_id, metadata_ipfs);
+      let mint_op = mint_token(token_id, to_address, amount);
+      batch.withContractCall(create_op);
+      batch.withContractCall(mint_op);
+      return true;
+    },
+    create_and_mint_multiple: function({ token_id, metadata_ipfs, destinations }, batch) {
+      let create_op = create_token(token_id, metadata_ipfs);
+      let mint_args = destinations.map(e => ({ token_id, owner: e.to_address, amount: e.amount }));
+      let mint_op = contract_ops.mint_tokens(mint_args);
+      batch.withContractCall(create_op);
+      batch.withContractCall(mint_op);
+      return true;
+    },
+    get_next_from_table: get_next_from_table,
+    mint: function({ token_id, to_address, amount }, batch) {
+      let mint_op = mint_token(token_id, to_address, amount);
+      batch.withContractCall(mint_op);
+      return true;
+    },
+    transfer: transfer,
+  };
 }
