998f02939b6b86f9d6a9e9d0083b0c1434c82ca8
Joris Röling <joris@bonboa.com>
2022-11-02 14:04:19 +0100

    PEPPERMINT_CONFIG

--- confloader.mjs
+++ confloader.mjs
@@ -4,6 +4,11 @@ const require = createRequire(import.meta.url);
 const CONFIG_PATH = '.'
 
 export default function() {
+  let config_json = process.env.PEPPERMINT_CONFIG;
+  if(config_json) {
+    console.log(config_json);
+    return JSON.parse(config_json);
+  }
 	let profile = process.env.PEPPERMINT_PROFILE;
 	let config_filename = profile ? `config_${profile}.json` : 'config.json';
 	console.log(`Reading configuration from ${config_filename}...`);
--- queue.mjs
+++ queue.mjs
@@ -8,6 +8,7 @@ const { MongoClient, ObjectID } = require('mongodb')
 const CHECKOUT_SQL = "WITH cte AS (SELECT id FROM peppermint.operations WHERE state='pending' AND originator=$1 ORDER BY id ASC LIMIT $2) UPDATE peppermint.operations AS op SET state = 'processing' FROM cte WHERE cte.id = op.id RETURNING *";
 const SENT_SQL = "UPDATE peppermint.operations SET included_in = $1 WHERE id = ANY($2)"
 const SET_STATE_SQL = "UPDATE peppermint.operations SET state = $1 WHERE id = ANY($2)"
+
 const KILL_CANARIES_SQL = "DELETE FROM peppermint.operations WHERE state='canary' AND originator = $1"
 
 export default async function(db_connection) {
